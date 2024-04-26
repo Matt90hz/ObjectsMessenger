759c4df79ba86bd8bb0853e6eec02d4f6e4891bc
noname <noname@nomail>
2024-04-26 18:10:24 +0200

    Implemented SetAndSend

--- IncaTechnologies.ObjectsMessenger/Extensions/MessengerExtensions.cs
+++ IncaTechnologies.ObjectsMessenger/Extensions/MessengerExtensions.cs
@@ -1,14 +1,74 @@
 ﻿using IncaTechnologies.ObjectsMessenger;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace IncaTechnologies.ObjectsMessenger
 {
+    /// <summary>
+    /// Utility extensions for <see cref="Messenger"/>.
+    /// </summary>
     public static class MessengerExtensions
     {
-        public static void SendProperty<TSender, TReceiver, TMessage>(this Messenger<TSender, TReceiver, TMessage> messenger, TSender sender, ref TMessage field, TMessage value)
+        /// <summary>
+        /// Utility to set a property backing field and send the new value as a message in one line.
+        /// <example>
+        /// <code>
+        /// class MyClass
+        /// {
+        ///     private readonly ObjectMessenger _objectMessenger;
+        ///     private object _myProperty;
+        ///     
+        ///     public object MyProperty
+        ///     {
+        ///         get => _myProperty;
+        ///         set => _objectMessenger.SetAndSend(this, ref _myProperty, value);
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
+        /// </summary>      
+        /// <typeparam name="TSender"></typeparam>
+        /// <typeparam name="TReceiver"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="messenger"></param>
+        /// <param name="sender"></param>
+        /// <param name="field">Reference to the backing field of the property.</param>
+        /// <param name="value">Incoming value for the property.</param>
+
+        public static void SetAndSend<TSender, TReceiver, TMessage>(this Messenger<TSender, TReceiver, TMessage> messenger, TSender sender, ref TMessage field, TMessage value)
+        {
+            field = value;
+            messenger.Send(sender);
+        }
+
+        /// <summary>
+        /// Utility to set a property backing field and send the new value as a message in one line.
+        /// <example>
+        /// <code>
+        /// class MyClass
+        /// {
+        ///     private readonly ObjectMessenger _objectMessenger;
+        ///     private object _myProperty;
+        ///     
+        ///     public object MyProperty
+        ///     {
+        ///         get => _myProperty;
+        ///         set => _objectMessenger.SetAndSend(this, ref _myProperty, value);
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
+        /// </summary> 
+        /// <typeparam name="TSender"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="messenger"></param>
+        /// <param name="sender"></param>
+        /// <param name="field">Reference to the backing field of the property.</param>
+        /// <param name="value">Incoming value for the property.</param>
+        public static void SetAndSend<TSender, TMessage>(this Messenger<TSender, TMessage> messenger, TSender sender, ref TMessage field, TMessage value)
         {
             field = value;
             messenger.Send(sender);
