597cfb2c8838dd02be51a8d8d34cb85ddf321962
noname <noname@nomail>
2024-04-30 17:13:24 +0200

    Moved to file

---
+++ Tests/MessengerHubMessengersErrorsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using IncaTechnologies.ObjectsMessenger;
+using System.Reactive.Linq;
+using Tests.Utililties;
+
+namespace Tests;
+
+public sealed class MessengerHubMessengersErrorsTests
+{
+    [Fact]
+    public void WhenMessengersAreSubscribed_ErrorsShouldBeReemittedByMesssengerHub()
+    {
+        //arrange
+        Sender sender = new();
+        Receiver receiver = new();
+        GuidMessenger messenger1 = new();
+        GuidMessenger messenger2 = new();
+        MessengerHub messengerHub = (MessengerHub)Activator.CreateInstance(typeof(MessengerHub), true)!;
+
+        messengerHub.RegisterMessenger(messenger1);
+        messengerHub.RegisterMessenger(messenger2);
+
+        List<(Messenger, Exception)> errorsEmittedByMessengers = [];
+        List<(Messenger, Exception)> errorsEmittedByHub = [];
+
+        messengerHub.MessengersErrors.Subscribe(ex => errorsEmittedByHub.Add(ex));
+        messenger1.Errors.Subscribe(ex => errorsEmittedByMessengers.Add((messenger1, ex)));
+        messenger2.Errors.Subscribe(ex => errorsEmittedByMessengers.Add((messenger2, ex)));
+
+        //act
+        messenger1.Receive(receiver);
+        messenger2.Receive(receiver);
+        messenger1.Send(sender);
+        messenger2.Send(sender);
+        messenger1.Receive(receiver);
+        messenger1.Receive(receiver);
+        messenger2.Receive(receiver);
+        messenger2.Receive(receiver);
+
+        //assert
+        errorsEmittedByHub.Should().Equal(errorsEmittedByMessengers);
+    }
+}
---
+++ Tests/MessengerHubRegisterMessengerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using IncaTechnologies.ObjectsMessenger;
+using System.Reactive.Linq;
+using Tests.Utililties;
+
+namespace Tests;
+
+public sealed class MessengerHubRegisterMessengerTests
+{
+    [Fact]
+    public void WhenMessengerIsRegistered_MessengerHubEventAndErrorsShouldBeEmitted()
+    {
+        //arrange
+        Sender sender = new();
+        Receiver receiver = new();
+        GuidMessenger messenger = new();
+        MessengerHub messengerHub = (MessengerHub)Activator.CreateInstance(typeof(MessengerHub), true)!;
+
+        messengerHub.RegisterMessenger(messenger);
+
+        using var monitorEvents = messengerHub.MessengersEvents.ToEvent().Monitor();
+        using var monitorErrors = messengerHub.MessengersErrors.ToEvent().Monitor();
+
+        //act
+        messenger.Send(sender);
+        messenger.Receive(receiver);
+        messenger.Receive(receiver);
+
+        //assert
+        monitorEvents.Should().Raise(nameof(monitorEvents.Subject.OnNext));
+        monitorErrors.Should().Raise(nameof(monitorEvents.Subject.OnNext));
+    }
+}
